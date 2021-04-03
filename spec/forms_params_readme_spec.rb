920c124a1148baf78009e46c7e767cdc828ed8b1
Adam Walker <adamw207@yahoo.com>
2021-04-03 17:19:06 +0000

    Done.

--- spec/forms_params_readme_spec.rb
+++ spec/forms_params_readme_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper'
 
 describe "App" do
-  describe 'GET /food_form' do
-    it "can fill in the form" do
-      agent = "Carl Jr."
-      food = "Vegan Black-Bean Burger"
-
-      visit "/food_form"
-
-      fill_in("name", with: agent)
-      fill_in("favorite_food", with: food)
-
-      expect(find_field('name').value).to eq agent
-      expect(find_field('favorite_food').value).to eq food
-    end
-  end
-
   describe 'POST /food' do
     it 'responds with a 200' do
       post '/food'
@@ -31,7 +16,7 @@ describe "App" do
 
       post '/food', params
 
-      expect(last_response.body).to eq("My name is #{params[:name]}, and I love #{params[:favorite_food]}.")
+      expect(last_response.body).to eq("My name is #{params[:name]}, and I love #{params[:favorite_food]}")
     end
   end
 end
